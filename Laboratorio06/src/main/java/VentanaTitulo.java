6c008eafe05d96dbaedc520ac634f86a936ee435
brisendiarufinoalama@gmail.com <IDAT@DESKTOP-T81UHG4>
2025-02-17 10:02:25 -0500

    Ventana

---
+++ Laboratorio06/src/main/java/VentanaTitulo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author IDAT
+ */
+public class VentanaTitulo extends Ventana{
+    private String titulo;
+    
+    public VentanaTitulo(int x, int y, int ancho, int alto, String titulo){
+        super(x, y, ancho, alto);
+        this.titulo = titulo;
+        
+    public void mostrar() {
+    }
+    
+}
